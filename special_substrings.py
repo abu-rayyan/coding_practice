c4cfbeea976e98b754cf8d29980ab627a83c4763
abu-rayyan <sajidurrehman1@gmail.com>
2022-08-22 13:00:59 +0500

    executed special strings problem

---
+++ special_substrings.py
@@ -0,0 +1,32 @@
+
+s="aaaabaaaa"
+# s="aasasd"
+n=len(s)
+result = 0
+i = 0
+
+
+while (i < n):
+    char_count = 1
+    while (i + 1 < n and s[i] == s[i+1]):
+        i += 1
+        char_count += 1
+
+    result += int(char_count * (char_count + 1) / 2)
+    i += 1
+
+for i in range(1, n):
+    char_count = 1
+    while (
+        i + char_count < n and
+        i - char_count >= 0 and
+        s[i] != s[i-1] and
+        s[i-char_count] == s[i+char_count] and
+        s[i-1] == s[i-char_count]
+    ):
+        print ("character is:", s[i])
+        char_count += 1
+
+    result += char_count - 1
+
+# return result
